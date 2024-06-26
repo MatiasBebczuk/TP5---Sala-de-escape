207cfc31b136aab2263a7159bc366a7b15cdad65
Android Studio <devandroid@ort>
2024-06-26 09:03:08 -0300

    HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,30 +2,62 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using TP5___Sala_de_escape.Models;
 
-namespace TP5___Sala_de_escape.Controllers;
-
-public class HomeController : Controller
+namespace TP5___Sala_de_escape.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Tutorial()
+        {
+            return View();
+        }
+
+        public IActionResult Comenzar()
+        {
+            return View();
+        }
+
+        public IActionResult Habitacion(int sala, string clave)
+        {
+            bool salaCorrecta = true;
+            bool claveCorrecta = true;
+
+            if (salaCorrecta == false)
+            {
+
+                return RedirectToAction("Comenzar"); 
+            }
+
+            if (claveCorrecta == false)
+            {
+
+                ViewBag.Error = "La respuesta escrita fue incorrecta."; 
+                return View(); 
+            }
+
+            return View("Victoria");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
