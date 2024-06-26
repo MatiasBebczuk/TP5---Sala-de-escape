7aa37d280c2a8dd1a68565011f0478fc81d1a686
MATÍAS ILÁN BEBCZUK <48242936@ort>
2024-06-26 08:16:08 -0300

    Inicializacion

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP5___Sala_de_escape.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
