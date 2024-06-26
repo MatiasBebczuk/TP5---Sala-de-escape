c63f1326f4dc26b91a0485fd23ad403dc8c3aca8
MatiasBebczuk <48242936@est.ort.edu.ar>
2024-06-26 08:33:43 -0300

    Models

---
+++ Models/Escape.cs
@@ -0,0 +1,15 @@
+class Escape{
+    static string[] incognitasSalas;
+    static int estadoJuego;
+    private static void InicializarJuego(){
+
+    }
+    public static int GetEstadoJuego(){
+
+        return 2;
+    }
+    public static bool ResolverSala(int Sala, string Incognita){
+
+        return false;
+    }
+}
